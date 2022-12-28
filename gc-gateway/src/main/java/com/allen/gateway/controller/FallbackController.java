75af6635e611effe5276ccc5120495c0b0caa3d9
xuguocai <xuguocai@co-mall.com>
2022-12-28 21:55:05 +0800

    从网关获取注册的服务

--- gc-gateway/src/main/java/com/allen/gateway/controller/FallbackController.java
+++ gc-gateway/src/main/java/com/allen/gateway/controller/FallbackController.java
@@ -1,7 +1,11 @@
 package com.allen.gateway.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * 熔断后转发处理类
@@ -10,9 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FallbackController {
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/fallback")
     public String fallback() {
         return "fallback";
+    }
+
+    @GetMapping("/get")
+    public List<String> get() {
+        List<String> services = discoveryClient.getServices();
+        return services;
     }
 
 }
