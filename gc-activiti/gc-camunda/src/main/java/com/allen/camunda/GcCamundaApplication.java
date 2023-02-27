6f9a91c790c119b39cec1d9a276a229737205e3a
allen <xuguocai@co-mall.com>
2023-02-27 17:13:31 +0800

    增加camunda 框架工作流

---
+++ gc-activiti/gc-camunda/src/main/java/com/allen/camunda/GcCamundaApplication.java
@@ -0,0 +1,13 @@
+package com.allen.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcCamundaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcCamundaApplication.class, args);
+	}
+
+}
